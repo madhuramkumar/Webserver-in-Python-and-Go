182f20a8a0bdce4f0029f2cb564874d88c7d5fb1
Rohit Ramkumar <rohitramkumar@Rohits-MBP.fios-router.home>
2021-03-12 09:53:52 -0500

    remove comments

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,2 @@
-print("hello world")
-print("my name is madhumita ramkumar and I can't make a file")
-print ("my dog's name is waldo")
-print ("rohit is annoying")
-
 person = input('Enter your name: ')
 print('Hello', person)
