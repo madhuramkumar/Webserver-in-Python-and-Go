61a6398ac4de89903470563061e9d4a791648391
madhuramkumar <ramkumarmadhumita@gmai.com>
2021-03-12 08:08:59 -0800

    yay

--- hello_world.py
+++ hello_world.py
@@ -3,5 +3,5 @@ print("my name is madhumita ramkumar and I can't make a file")
 print ("my dog's name is waldo")
 print ("rohit is annoying")
 
-person = input('Enter your name: ')
-print('Hello', person)
+name = raw_input("Enter your name: ")
+print("Hello " + name + "!")
