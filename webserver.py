3deaca35df5ac31084e7ccdd1ee7cb5afce46ec3
madhuramkumar <ramkumarmadhumita@gmail.com>
2021-03-16 14:59:57 -0700

    webserver with new url path

--- webserver.py
+++ webserver.py
@@ -1,11 +1,25 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class helloHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
+
+    def do_YAY(self):
         self.send_response(200)
         self.send_header('content-type', 'text/html')
         self.end_headers()
         self.wfile.write('Hello World!'.encode())
+
+    def do_FOOBAR(self):
+        self.send_response(200)
+        self.send_header('content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write('foobar'.encode())
+
+
+    def do_GET(self):
+        if self.path == '/':
+            do_YAY(self)
+        elif self.path == '/foobar':
+            do_FOOBAR(self)
 
 def main():
     PORT = 8080
